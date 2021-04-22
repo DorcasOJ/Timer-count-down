13ee5b6625ff5f39ea638a732ecbe6b494a6d295
Dorcas <ayanfedorcas2017@gmail.com>
2021-04-22 20:50:08 +0100

    timer

--- app/templates/commands.py
+++ app/templates/commands.py
@@ -1,9 +1,13 @@
 import click
+from app import app
 from flask.cli import with_appcontext
 from app import db
 
 
 @click.command(name = 'create_tables')
+#@app.cli.command('create_tables')
 @with_appcontext
 def create_tables():
     db.create_all()
+    print('Database created')
+
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from app.app import *
+from app.command import create_tables
+
+app.cli.add_command(create_tables)
 
 if __name__ == "__main__":
     db.create_all()
